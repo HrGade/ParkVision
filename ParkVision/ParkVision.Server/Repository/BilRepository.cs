67a1cb902799a5f232392e66d83eb1681d26907d
kleethesama <38240805+kleethesama@users.noreply.github.com>
2025-05-17 19:52:14 +0200

    Done with unit tests and refactored repository.

--- ParkVision/ParkVision.Server/Repository/BilRepository.cs
+++ ParkVision/ParkVision.Server/Repository/BilRepository.cs
@@ -15,11 +15,21 @@ public class BilRepository : IBilRepository
     public async Task<Bil?> GetByIdAsync(string id)
     {
         Bil? bil = _biler.Find(e => e.Nummerplade == id);
+        if (bil == null)
+        {
+            Console.WriteLine($"Kunne ikke finde bilen med nummerplade: {id}");
+        }
         return await Task.FromResult(bil);
     }
 
     public async Task<Bil?> AddAsync(Bil bil)
     {
+        if (bil.Nummerplade.Length > 7)
+        {
+            throw new ArgumentException("Nummerpladen på bilen har mere end 7 tegn:" +
+                $" {bil.Nummerplade} -> {bil.Nummerplade.Length} tegn",
+                nameof(bil));
+        }
         if (await ExistsAsync(bil.Nummerplade))
         {
             Console.WriteLine($"En Bil har allerede dette id: {bil.Nummerplade}");
@@ -34,7 +44,6 @@ public class BilRepository : IBilRepository
         Bil? bilToBeDeleted = await GetByIdAsync(id);
         if (bilToBeDeleted == null)
         {
-            Console.WriteLine($"Kunne ikke finde bilen med nummerplade: {id}");
             return null;
         }
         _ = _biler.Remove(bilToBeDeleted);
@@ -43,10 +52,9 @@ public class BilRepository : IBilRepository
 
     public async Task<Bil?> UpdateAsync(string id, Bil bil)
     {
-        Bil? bilToBeUpdated = await GetByIdAsync(bil.Nummerplade);
+        Bil? bilToBeUpdated = await GetByIdAsync(id);
         if (bilToBeUpdated == null)
         {
-            Console.WriteLine($"Kunne ikke finde bilen med nummerplade: {bil.Nummerplade}");
             return null;
         }
         bilToBeUpdated.Nummerplade = bil.Nummerplade;
@@ -55,7 +63,6 @@ public class BilRepository : IBilRepository
 
     public async Task<IEnumerable<Bil>> GetAllAsync()
     {
-        IEnumerable<Bil> result = [.. _biler];
-        return await Task.FromResult(result);
+        return await Task.FromResult((IEnumerable<Bil>)[.. _biler]);
     }
 }
