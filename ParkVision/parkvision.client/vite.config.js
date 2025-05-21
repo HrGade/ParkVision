e55f2fb6fba47d5768fb9d76081925f5ae9e1455
Frederik Christian Klee <frederikchrklee@gmail.com>
2025-05-21 10:18:05 +0200

    Removed button and new hosting options. (#9)
    * Changing interface name to be more generic.
    
    * Started implementing last repository for testing.
    
    * Repository is finished, but needs to be unit tested.
    
    * More work on repository and tests.
    
    * Removed 3rd party api from database schema. Also fixed bug with dbContext tracking entities.
    
    * More files to ignore.
    
    * Changed update (put), but will have to remove this since we can't update bil anyway.
    
    * Bil controller and DB repository are done.
    
    * Forgot to check if the bil exist in database when updating.
    
    * Forgot to make the repository the one to check if a bil exists in DeleteBil.
    
    * Done with test repository.
    
    * Removed useless comment.
    
    * Made the first batch of unit tests.
    
    * Wrong comment.
    
    * Keeping
    
    * Merge branch 'main' into application-frederik
    
    * I doubt we need to track this file.
    
    * Changed file name be to have clearer name.
    
    * Ignoring test log file type.
    
    * Done with unit tests and refactored repository.
    
    * Now it should be done for real since I figured out how controller will handle an invalid nummerplade.
    
    * Should be completely done with repository + unit tests + bil controller.
    
    * Compiler should control string.
    
    * Ignoring yet another test file.
    
    * Started working on parkerings controller and making the API better.
    
    * Parkering controller works well with swagger and gives us the correct JSON format we want.
    
    * Axios works with our own API now.
    
    * Front-end is now seperated into vue components and we can get data from our 3rd party API now.
    
    * Done with parkering controller and its DTO models. Added a DTO model to biler as well.
    
    * Installed axios via npm (should it really bring all of these files in?).
    
    * Changed the bil JSON schema to fit with the rest of the API.
    
    * Enabled XML documentation generation for server project.
    
    * PUT didn't return the actually changed bil, but does now.
    
    * Trying to get the front end to function properly.
    
    * Last front-end implementations (that we have time for) and using simply.com connection string.
    
    * Wrote some comments for models and a bit for parkering controller.
    
    * Public host (Frederik) and removed "Tag nyt billede"-button.

--- ParkVision/parkvision.client/vite.config.js
+++ ParkVision/parkvision.client/vite.config.js
@@ -39,13 +39,14 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
+  plugins: [plugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+        host: '0.0.0.0',
         proxy: {
             '^/api': {
                 target,
